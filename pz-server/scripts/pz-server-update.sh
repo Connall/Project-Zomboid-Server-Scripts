f80ca8bec5cba18b8e13f98be8a0bf1e8105c1ac
Connall Lindsay <connall@auldbreck.com>
2014-04-06 15:19:07 +0100

    Added License info.

--- pz-server/scripts/pz-server-update.sh
+++ pz-server/scripts/pz-server-update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Scripts are licensed under Attribution-NonCommercial 4.0 International. Commercial permission must be seeked by original author.
 #Make sure script is run on debian wheezy
 #Script created by Connall Lindsay, Assistance by Kirrus
 if grep -q "Linux 7" /etc/issue; then
